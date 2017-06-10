f7498e609f4aacbaa39e5a98284f5253d8f57fb8
Liaizon Wakest <wakest@gmail.com>
2017-06-10 18:40:56 -0500

    uptades

---
+++ backup.js
@@ -0,0 +1,43 @@
+//backup
+
+var favColor = 'blue';
+var myFavColors = ['blue', 'red', 'green'];
+var numOfFavColors = 3;
+var hasGotFavColors = true;
+var richObject = {
+	firstName: 'Liaizon',
+	lastName: 'Wakest',
+	favColor: ['blue', 'red', 'green'],
+	yearsAlive: 29,
+	isAlive: true
+};
+
+
+
+var fullname = function (firstname, secondname) {
+return firstname + ' ' + secondname
+};
+
+var whatIsMyFavColor = function () {
+	return 'blue';
+	return true;
+};
+
+var doSomething = function () {
+	console.log("do something!");
+
+
+var area = function (width, height) {
+	return width * height;
+};
+
+
+
+
+
+//   || means or 
+
+// && means and
+
+// == means equils
+
---
+++ explanations.js
@@ -0,0 +1,11 @@
+var name1 = 'Liaizon';
+var name2 = 'Bob';
+
+if (name1 == 'Liaizon') {
+	alert('true 1!!');
+} else if (name2 == 'Liaizon') {
+	alert('true 2!!!');
+} else {
+	alert('false!!!');
+};
+
