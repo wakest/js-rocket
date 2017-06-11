4a725d96379ca81f1f9082382d3e168975d5c1a3
Liaizon Wakest <wakest@gmail.com>
2017-06-10 22:24:52 -0500

    made my rocket

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+var doCoolStuff = function () {
+	var currentClassName = document.getElementById('cool').className;
+
+if (currentClassName == 'cool') {
+	document.getElementById ('cool').className = 'cool red';
+
+} else {
+		document.getElementById('cool').className = 'cool';
+	}
+}
+
+
+
+
+
+
+
+var sayMyName = function (name) {
+	alert('My name is: '+name);
+}
+		
 		var car = {
 			make: 'VW',
 			type: 'Polo',
@@ -17,7 +38,7 @@
 				alert('fly');
 			},
 			switchCar: function (isOn) {
-					console.log('turn car on'+isOn);
+					console.log('turn car on'+ isOn);
 					if (isOn == true) {
 						this.isTurnedOn = true;
 					} else {
